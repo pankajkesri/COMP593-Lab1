2a95f83b64db4d13bc0cdb14aeb69ba8a2b99cba
pankajkesri <pkesri@flemingcollege.ca>
2024-01-16 11:09:53 -0500

    Modified the both files in repo clone
    deleted the one print line in script1.py
    added the one print line in script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Although practicality beats purity.")
 print("Special cases aren't special enough to break the rules.")
+print("Although never is often better than *right* now.")
